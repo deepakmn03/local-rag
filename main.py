7c80e56ca5c7648faec3d3e05f76f2c40a2cbf61
deepakmn03 <meenadeepu22@gmail.com>
2026-05-10 01:57:18 +0530

    Add review data and implement retrieval-based Q&A for running watches
    - Created a new CSV file `data.csv` containing reviews for Garmin and Coros running watches.
    - Developed a main script `main.py` that utilizes Langchain to answer questions based on the reviews.
    - Implemented a vector database in `vector.py` to store and retrieve review documents using embeddings.
    - Added necessary dependencies to `requirements.txt` for Langchain and related libraries.

---
+++ main.py
@@ -0,0 +1,28 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+
+You are an expert in answering the questions about sports running watches garmin 165, 265, coros pace 3 and coros pace 4.
+
+Here are some relevant reviews: {reviews}
+Here is the questio to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model 
+
+while True:
+    print("\n----------------------\n")
+    question = input("Ask your question (q for quit): ")
+    if question == "q":
+        break
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
+
+
